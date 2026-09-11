966d26efe18bd4bd13f3e4eedf34b9f18a6ca01c
skyokos7 <skyokos7@gmail.com>
2026-09-11 09:24:01 +0800

    Create market-data.js

---
+++ futures-signal-app-v1/market-data.js
@@ -0,0 +1,20 @@
+// 海外期貨紅綠燈 App
+// 市場資料設定 V1
+
+const MARKET_SYMBOLS = {
+  NQ: "NQ",
+  YM: "YM",
+  EUR: "6E",
+  GC: "GC",
+  HG: "HG",
+  ZS: "ZS",
+  ZL: "ZL",
+  ZM: "ZM",
+  ZW: "ZW",
+  ZC: "ZC",
+  CL: "CL",
+  BZ: "BZ",
+  NG: "NG"
+};
+
+console.log("市場資料模組已載入", MARKET_SYMBOLS);
